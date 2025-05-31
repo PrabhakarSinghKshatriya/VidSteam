d260d7ae2078d9cfafc0acbfbfdc9607567ded61
Prabhakar Singh Kshatriya <prabhakarsingh5668@gmail.com>
2025-05-31 15:13:01 +0530

    new changes

--- Models/Video.js
+++ Models/Video.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose;
 
-const videoSchema = new mongoose.Schema({
+const videoSchema = new Schema({
     _id:mongoose.Schema.Types.ObjectId,
     title:{type:String,required:true},
     description:{type:String,required:true},
