e2a36391011d59f48b517e1ada997093e97e3641
Prabhakar Singh Kshatriya <prabhakarsingh5668@gmail.com>
2025-05-31 15:53:02 +0530

    change

--- Models/Comment.js
+++ Models/Comment.js
@@ -3,7 +3,7 @@ const { Schema } = mongoose;
 
 const commentSchema = new mongoose.Schema({
     _id:mongoose>Schema.Types.ObjectId,
-    user_id:{type:mongoose.Schema.Types.Objected,required:true,ref:'user'},
+    user_id:{type:mongoose.Schema.Types.ObjectId,required:true,ref:'user'},
     videoId:{type:String,required:true},
     commentText:{type:String,required:true}
 
